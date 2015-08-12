3afcdb3c968b2232330f72f35aad72e9458b2ae2
Tiago Justino <tiago.vmj@gmail.com>
2015-08-12 16:16:28 -0700

    Skip Links documentation added.

--- docs/src/develop/Accessibility.js
+++ docs/src/develop/Accessibility.js
@@ -25,7 +25,7 @@ var Accessibility = React.createClass({
           <p>This section describes the Grommet guidelines for developing accessibile applications.</p>
 
           <h3>Icons</h3>
-          <p>The icon components can be read by screen readers.  The default textual description for icons can be overridden by setting the a11yTitle.  Both the default or a11yTitle attribute use localization if it exist.</p>
+          <p>The icon components can be read by screen readers. The default textual description for icons can be overridden by setting the a11yTitle. Both the default or a11yTitle attribute use localization if it exist.</p>
           <h4>Example:</h4>
           <Table>
             <caption>Example of Icons with differnt values</caption>
@@ -60,7 +60,31 @@ var Accessibility = React.createClass({
             {"<App lang=\"en-US\">\n  ...\n</App>"}
           </code></pre>
           <h3>Skip Links</h3>
-          <p>Spik links allow blah</p>
+          <p>Spik links allow screen-readers and keyboard-only users to skip
+          the repetitive content that is usually in the begining of a page,
+          such as headers and navigation bars. By using it, your user is
+          capable of jumping directly to the main content of your page or to
+          the footer.</p>
+          <p>The skip links come by default with grommet App. The "Skipt to
+          Footer" link is added by default when you use the Footer component.
+          In order to add the "Skip to Main Content" link you need to point
+          what is the main content of your page. This is done by setting the
+          attribute <code>primary="true"</code> in the Article or Section
+          component as shown below:</p>
+          <h4>Example:</h4>
+          <pre><code className="html">
+            {"<App>\n" +
+            "  <Article>\n" +
+            "    <Header>\n" +
+            "      <h1>Title</h1>\n" +
+            "    </Header>\n" +
+            "    <Section primary={true}>\n" +
+            "      <h2>Heading</h2>\n" +
+            "      <p>Lorem ipsum ...</p>\n" +
+            "    </Section>\n" +
+            "  </Article>\n" +
+            "</App>"}
+          </code></pre>
         </Section>
       </Article>
     );
